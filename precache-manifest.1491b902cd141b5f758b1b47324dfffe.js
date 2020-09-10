a439d976cf7036db9703fc240992a38e5763efe2
MuratYavuzz <muraatyavuz@gmail.com>
2020-09-10 08:52:41 +0300

    Updates

---
+++ precache-manifest.1491b902cd141b5f758b1b47324dfffe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7909743b4d58b88e712bd67099cd70",
+    "url": "/CocktailDatabase/index.html"
+  },
+  {
+    "revision": "36855ae2e604a7611700",
+    "url": "/CocktailDatabase/static/css/2.ee47ed0b.chunk.css"
+  },
+  {
+    "revision": "36855ae2e604a7611700",
+    "url": "/CocktailDatabase/static/js/2.69095bff.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/CocktailDatabase/static/js/2.69095bff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6613730467072235b062",
+    "url": "/CocktailDatabase/static/js/main.d3af507d.chunk.js"
+  },
+  {
+    "revision": "66489362231747908ea5",
+    "url": "/CocktailDatabase/static/js/runtime-main.8cdd5b2b.js"
+  }
+]);
